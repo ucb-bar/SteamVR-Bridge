28c10d815ccb81caab594feb3145c384ea3e2dff
-T.K.- <tk233.xyz@gmail.com>
2026-03-01 22:07:17 -0800

    add: example script from xr_examples

---
+++ scripts/openxr_examples/track_hmd2.py
@@ -0,0 +1,44 @@
+"""
+pyopenxr example program track_hmd2.py
+
+Prints the position of your head-mounted display for 30 frames.
+
+From https://github.com/cmbruns/pyopenxr_examples/blob/main/xr_examples/track_hmd2.py
+"""
+
+import time
+import xr
+from xr.utils.gl import ContextObject
+from xr.utils.gl.glfw_util import GLFWOffscreenContextProvider
+
+# ContextObject is a high level pythonic class meant to keep simple cases simple.
+with ContextObject(
+    context_provider=GLFWOffscreenContextProvider(),
+    instance_create_info=xr.InstanceCreateInfo(
+        enabled_extension_names=[
+            # A graphics extension is mandatory (without a headless extension)
+            xr.KHR_OPENGL_ENABLE_EXTENSION_NAME,
+        ],
+    ),
+) as context:
+    # Loop over the render frames
+    for frame_index, frame_state in enumerate(context.frame_loop()):
+        view_state, views = xr.locate_views(
+            session=context.session,
+            view_locate_info=xr.ViewLocateInfo(
+                view_configuration_type=context.view_configuration_type,
+                display_time=frame_state.predicted_display_time,
+                space=context.space,
+            )
+        )
+        flags = xr.ViewStateFlags(view_state.view_state_flags)
+        if flags & xr.ViewStateFlags.POSITION_VALID_BIT:
+            view = views[xr.utils.Eye.LEFT]
+            print(view.pose, flush=True)
+        else:
+            print("pose not valid")
+        # Slow things down, especially since we are not rendering anything
+        time.sleep(0.05)
+        # Don't run forever
+        if frame_index > 30:
+            break
